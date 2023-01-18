c295dccaa01c9f84df8415ab5e4ff8e586e1889f
fazlidddinKhayrullaev11 <fazliddinkhayrullaev9@gmail.com>
2023-01-18 23:59:03 +0500

    Binary6

--- Binary-Search/binary6.js
+++ Binary-Search/binary6.js
@@ -1,34 +1,29 @@
 
 
 
-       let numbers=[0,1,2,3,4,5,6,7,8,9,10];
+
+let nums=[0,1,2,3,4,5,6,7,8,9,10]
+
+let binary=(arr,val)=>{
+ let lower=0;
+ let upper=arr.length-1;
 
 
-       let binary=(arr,val)=>{
+ while (lower<=upper) {
+ console.log('attempts')
+    let middle=lower+Math.floor((upper-lower)/2);
 
-       let start=0;
-       let end=arr.length-1;
-       
-       while(start<=end){
-         let middle=start+Math.floor((end-start)/2);
-
-          if(val===arr[middle]){
-          return middle;
-
-           }
-
-          if (val<arr[middle]){
-             upper=middle-1;
+    if (val===arr[middle]) {
+        return middle
         
-       
-           }
-          else{
+    }
+    if (val<arr[middle]) {
+        upper=middle-1
+        
+    } else {
+        lower=middle+1;
+    }
+    }
+}
 
-            lower=middle+1;
-
-            }
-
-             }
-
-             }
-            console.log(binary(numbers,2));
+console.log(binary(nums,1));
